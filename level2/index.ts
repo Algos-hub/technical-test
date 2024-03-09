c44c51d405eb723dd368405db1b3da5ae5e106cf
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2024-03-09 14:43:35 +0100

    level 2 + fixed implicit typing in level 1

--- level2/index.ts
+++ level2/index.ts
@@ -1,7 +1,7 @@
 // offloading interfaces to a different file so only the actual logic is present in this file
 import { Car, Rental, Rental_price } from "./interfaces";
 
-export function final_price_level_1(cars: Car[], rentals_arr: Rental[]): Rental_price[] {
+export function final_price_level_2(cars: Car[], rentals_arr: Rental[]): Rental_price[] {
 	// maps the rentals_arr array
 	const rentals: Rental_price[] = rentals_arr.map((rental: Rental) => {
 		// matches the car_id to its corresponding car obj in the cars array
@@ -12,7 +12,21 @@ export function final_price_level_1(cars: Car[], rentals_arr: Rental[]): Rental_
 
 		// calculate the total price per km and the total price per days based on the total days and the estimated distance travelled 
 		const total_price_per_km: number = rental.distance * car.price_per_km;
-		const total_price_per_days: number = total_days * car.price_per_day;
+
+		// initialize the total price per day
+		let total_price_per_days: number = car.price_per_day;
+
+		// initialise the discount multiplier
+		let discount: number = 1;
+
+		// loop through the number of days and apply the correct discount for the current day
+		for(let i = 1; i <= total_days; ++i) {
+			if(i > 1) discount = 0.1;
+			if(i > 4) discount = 0.3;
+			if(i > 10) discount = 0.5;
+			total_price_per_days += car.price_per_day - (car.price_per_day * discount);
+		}
+
 		
 		// returns the final rental object with the rental id and the final added price of the total price per days and the total price per km
 		return {id: rental.id, price: total_price_per_days + total_price_per_km}
